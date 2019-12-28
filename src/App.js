7a58734eeab77882c7a5106f1dd2594f1a879959
srikanthsri <aryansri910@gmail.com>
2019-12-28 16:31:45 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import React, { Component } from 'react'
 import Routing from './Routing'
 import Navbar from './Components/F3MainComponents/SubCompoents/F3_Navbar'
 import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './Components/Redux/store'
+
 
 export default class App extends Component {
   render() {
     return (
       <div>
         
-        <Provider store={store}>
+        
       <Routing/>
 
-      </Provider>
-     
+      
       </div>
     )
   }
